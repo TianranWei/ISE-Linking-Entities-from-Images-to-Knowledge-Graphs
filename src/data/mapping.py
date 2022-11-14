0b40143d0678b1e2bf77eda8ba7ebb719027da11
DavideAl <Davide.Alpino@googlemail.com>
2022-11-14 17:31:46 +0100

    added code for mapping

---
+++ src/data/mapping.py
@@ -0,0 +1,33 @@
+import logging
+
+import pandas as pd
+
+
+
+
+logger = logging.getLogger(__name__)
+class StringMatcher():
+    
+    def __init__(self) -> None:
+        self.df = pd.read_csv("data/raw/dbpedia_classes.csv")
+    
+    def match_string(self, string:str) -> pd.DataFrame:
+        """method that gives us the dbpedia class to a given string
+        OBS: the string needs the be the same as the label of the class
+        TODO: find pattern that allows upper/lowercase differences if necessary
+        Args:
+            string (str): the class we are looking for
+        Returns:
+            pd.DataFrame: returns a DataFrame with the corresponding classes from dbpedia
+        """        
+        output = ""
+        if self.df['label'].eq(string).any():
+            df = self.df[self.df['label'] == string]['class']
+            output = pd.DataFrame(df)
+            output = output.reset_index(drop=True)           
+        else:
+            logger.info("Couldnt find matching string to: {string}")
+        return output    
+    
+a = StringMatcher()
+print(a.match_string("zoo"))
